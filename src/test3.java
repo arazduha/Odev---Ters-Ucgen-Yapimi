4a3b5b2abdf7b9e36c466de81d0b05a32dc51ee8
arazduha <arazduha02@gmail.com>
2022-08-07 13:42:32 +0300

    java101 - odev3.3 - patika.dev

---
+++ src/test3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class test3 {
+    public static  void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak sayisi giriniz: ");
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * n - (2 * i + 1)); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
+
+
+
+
+
